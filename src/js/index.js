4195b13eb374f0197c8c2fd9079465ad05cf3793
Guilherme Gongora <guilhermegongora2007@gmail.com>
2023-10-01 16:50:33 -0300

    Unselecting button in a global scope function

--- src/js/index.js
+++ src/js/index.js
@@ -6,16 +6,15 @@ const personagens = document.querySelectorAll(".personagem");
 botoes.forEach((botao, indice) => {
     botao.addEventListener("click", () => {
         desselecionarBotao();
-        desselecionarPersonagem();
         botao.classList.add("selecionado");
+
+
+
+        const personagemSelecionado = document.querySelector(".personagem.selecionado");
+        personagemSelecionado.classList.remove("selecionado");
         personagens[indice].classList.add("selecionado");
     });
 })
-function desselecionarPersonagem() {
-    const personagemSelecionado = document.querySelector(".personagem.selecionado");
-    personagemSelecionado.classList.remove("selecionado");
-}
-
 function desselecionarBotao() {
     const botaoSelecionado = document.querySelector(".botao.selecionado");
     botaoSelecionado.classList.remove("selecionado");
